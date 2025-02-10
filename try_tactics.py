2985c0720c52bd7dcdbcecb87ff0075015ad30c3
attilajam <attila.jamilov@gmail.com>
2025-02-10 12:04:43 -0800

    search n tactics and log errors

--- try_tactics.py
+++ try_tactics.py
@@ -10,25 +10,25 @@ def parse_tactics():
         "exact hq"
     ]
     return tactics
-  
+
 if __name__ == '__main__':
     server = Server(project_path="./")
     state = server.goal_start("forall (p q: Prop), Or p q -> Or q p")
-  
+
     i = 0
-  
+
     tactics = parse_tactics()
-  
+
     while not state.is_solved:
         prev_state = state
         print(f"state{i}: {state} \n")
         tactic = tactics.pop(0)
-      
+
         try:
             new_state = server.goal_tactic(state, goal_id=0, tactic=tactic)
             i+= 1
             state = new_state
-          
+
         except Exception as e:
             tactics.append(tactic)
             print(e)
