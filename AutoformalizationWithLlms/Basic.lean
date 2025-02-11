7d05f939b58e152b2f2eb870f8df1a33b04db560
Elizabeth <116237434+elizabethyw@users.noreply.github.com>
2025-02-11 02:32:46 +0000

    add imports.

--- AutoformalizationWithLlms/Basic.lean
+++ AutoformalizationWithLlms/Basic.lean
@@ -1,1 +1,8 @@
+import Mathlib.Data.Nat.Factorization.Basic
+import Mathlib.Data.Nat.Prime
+import Mathlib.Data.Real.Basic
+
+open Function
+variable {α : Type _} (r s t : Set α)
+
 def hello := "world"
