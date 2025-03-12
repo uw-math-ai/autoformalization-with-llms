5535876c872a36f284058014ee903229f4938206
Siyuan Ge <jamesgsy@cs.washington.edu>
2025-03-12 14:44:32 -0700

    Removed the false evaluate code

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -11,9 +11,9 @@ max_steps = 20
 
 miniF2F_path = "./miniF2F-lean4"
 import_path = "./miniF2F-lean4/MiniF2F/Minif2fImport.lean"
-lean_path = "./data/test/fold_0.lean"
+lean_path = "./data/test/fold_6.lean"
 filename = os.path.basename(lean_path).rsplit(".lean")[0]
-csv_filename = f"{filename}_results_{max_steps}.csv"
+csv_filename = f"./data/results/{filename}_results_{max_steps}.csv"
 columns = ["theorem", "steps", "time", "success", "feedback", "proof"]
 
 server = create_minif2f_server(project_path=miniF2F_path, import_path=import_path)
@@ -27,24 +27,8 @@ results = []
 for name, goal in tqdm(goals.items()):
     start_time = time.time()
     try:
-        goal_state = create_goal_state(server, goal)
-        initial_state = AStarSearchState(
-            goal_state=goal_state
-        )
-    except Exception as e:
-        end_time = time.time()
-        result = {
-            "theorem": name,
-            "steps": 0,
-            "time": f"{(end_time - start_time):.2f}",
-            "success": "False",
-            "feedback": f"Failed to create goal state: {e}",
-            "proof": "",
-        }
-        results.append(result)
-        continue
-    try:
-        actions, solved, step, feedback = search_agent.search(initial_state, max_steps=max_steps, verbose=False)
+        sketch = goal.rsplit("sorry")[0].strip("\n ")
+        actions, solved, step, feedback = search_agent.search(sketch, max_steps=max_steps, verbose=False)
         end_time = time.time()
         if solved:
             proof = "\n".join([action.to_code() for action in actions])
@@ -65,6 +49,7 @@ for name, goal in tqdm(goals.items()):
                 "feedback": f"{feedback}",
                 "proof": "",
             }
+        print(result)
         results.append(result)
     except Exception as e:
         end_time = time.time()
